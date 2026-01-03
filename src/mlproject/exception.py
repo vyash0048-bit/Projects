e1f1ad16aabe67c312476bfa357d358196077b41
JOJO Verma <vyash0048@gmail.com>
2026-01-03 17:34:11 +0530

    Data ingestion

---
+++ src/mlproject/exception.py
@@ -0,0 +1,22 @@
+import sys
+from src.mlproject.logger import logging
+
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    return (
+        f"Error occurred in python script name [{file_name}] "
+        f"line number [{exc_tb.tb_lineno}] "
+        f"error message [{str(error)}]"
+    )
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_details)
+
+    def __str__(self):
+        return self.error_message
